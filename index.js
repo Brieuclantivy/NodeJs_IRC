c7a28bda31a5f1d801de83a13bc16909ba72113a
DE LANTIVY <brieuc.de-lantivy@sogeti.com>
2019-02-06 15:57:25 +0100

    [ADD] welcome message

--- index.js
+++ index.js
@@ -11,11 +11,27 @@ app.get('/', function(req, res){
 app.use(express.static('public'));
 
 io.on('connection', function(socket){
+        socket.on('nouveau_client', function(pseudo) {
+                socket.broadcast.emit('nouveau_client', pseudo + " has enter in the chat !");
+        });
+        
         socket.on('chat message', function(pseudo, msg){
                 console.log("MSG PSEUDO : " + pseudo);
                 io.emit('chat message', pseudo + " : " + msg);
         });
 });
+
+/**
+ * // join to subscribe the socket to a given channel (server-side):
+socket.join('some room');
+
+// then simply use to or in (they are the same) when broadcasting or emitting (server-side)
+io.to('some room').emit('some event'):
+
+// leave to unsubscribe the socket to a given channel (server-side)
+socket.leave('some room');
+
+ */
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
